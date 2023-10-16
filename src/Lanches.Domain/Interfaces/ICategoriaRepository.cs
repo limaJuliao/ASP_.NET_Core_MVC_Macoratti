9062ec391bed90e4a54d19059d8950da0b9bb014
Julio Lima <j.lima@t-systems.com>
2023-10-16 19:35:08 -0300

    [FEAT]: Categorias repository

---
+++ src/Lanches.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Lanches.Domain.Entities;
+
+namespace Lanches.Domain.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        public IEnumerable<Categoria> Categorias { get; }
+    }
+}
---
+++ src/Lanches.Infra.Data/Repositories/CategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using Lanches.Domain.Entities;
+using Lanches.Domain.Interfaces;
+using Lanches.Infra.Data.Context;
+
+namespace Lanches.Infra.Data.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly AppDbContext _context;
+    public CategoriaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IEnumerable<Categoria> Categorias => _context.Categorias;
+}
