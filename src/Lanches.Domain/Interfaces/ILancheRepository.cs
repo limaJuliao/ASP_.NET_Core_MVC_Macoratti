b8cdd3cf8c65af377ca1c004b365ae80a9b1dded
Julio Lima <j.lima@t-systems.com>
2023-10-16 19:44:27 -0300

    [FEAT]: Lanche repository

---
+++ src/Lanches.Domain/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lanches.Domain.Entities;
+
+namespace Lanches.Domain.Interfaces;
+
+public interface ILancheRepository
+{
+    IEnumerable<Lanche> Lanches { get; }
+    IEnumerable<Lanche> LanchesPreferidos { get; }
+    
+    Lanche GetById(int id);
+}
---
+++ src/Lanches.Infra.Data/Repositories/LancheRepository.cs
@@ -0,0 +1,22 @@
+﻿using Lanches.Domain.Entities;
+using Lanches.Domain.Interfaces;
+using Lanches.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches.Infra.Data.Repositories;
+
+public class LancheRepository : ILancheRepository
+{
+    private readonly AppDbContext _context;
+    public LancheRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IEnumerable<Lanche> Lanches => _context.Lanches.Include(x => x.Categoria);
+
+    public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+        .Where(l => l.IsLanchePreferido)
+        .Include(c=>c.Categoria);
+
+    public Lanche GetById(int id) => _context.Lanches.FirstOrDefault(x => x.Id == id);
+}
