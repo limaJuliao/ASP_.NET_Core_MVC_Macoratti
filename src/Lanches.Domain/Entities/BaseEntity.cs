ebb9adaaea110e8a56e06365db54b3af4426bac2
Julio Lima <j.lima@t-systems.com>
2023-10-16 13:51:42 -0300

    [FEAT]; Add domain class

---
+++ src/Lanches.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Lanches.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Lanches.Domain/Entities/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace Lanches.Domain.Entities
+{
+    public class Categoria : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+    }
+}
