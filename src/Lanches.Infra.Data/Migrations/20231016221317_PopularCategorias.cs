e12abbe53e2fafeb0eba9b75b4dfe91d70c26d15
Julio Lima <j.lima@t-systems.com>
2023-10-16 19:17:30 -0300

    [FEAT]: Populando a tabela categorias

---
+++ src/Lanches.Infra.Data/Migrations/20231016221317_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lanches.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO Categorias(Nome, Descricao)
+                                    VALUES('Normal', 'Lanche feito com ingredientes normais')");
+
+            migrationBuilder.Sql(@"INSERT INTO Categorias(Nome, Descricao)
+                                    VALUES('Natural', 'Lanche feito com ingredientes integrais e naturais')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
