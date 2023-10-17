b5b3664cd4592d3b7fc19ab4379b8d251beacb19
Julio Lima <j.lima@t-systems.com>
2023-10-16 22:27:55 -0300

    [FEAT]: View list lanche

---
+++ src/Lanches.Web/Configurations/LancheController.cs
@@ -0,0 +1,18 @@
+﻿using Lanches.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Web.Configurations;
+
+public class LancheController : Controller
+{
+    private readonly ILancheRepository _lancheRepository;
+    public LancheController(ILancheRepository lancheRepository)
+    {
+        _lancheRepository = lancheRepository;
+    }
+    public IActionResult List()
+    {
+        var lanches = _lancheRepository.Lanches;
+        return View(lanches);
+    }
+}
