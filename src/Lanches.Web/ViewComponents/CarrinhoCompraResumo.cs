dc9521e6b5641b2b9317636114e584e2c58ed9b9
Julio Lima <j.lima@t-systems.com>
2023-10-21 00:01:06 -0300

    {FEAT]: Criar viewcomponent para exibir detalhes do carrinho de compras

---
+++ src/Lanches.Web/ViewComponents/CarrinhoCompraResumo.cs
@@ -0,0 +1,27 @@
+﻿using Lanches.Application.Interfaces;
+using Lanches.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Web.ViewComponents;
+
+public class CarrinhoCompraResumo : ViewComponent
+{
+    private readonly ICarrinhoCompraAppService _carrinhoCompraAppService;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CarrinhoCompraResumo(ICarrinhoCompraAppService carrinhoCompraAppService, IHttpContextAccessor contextAccessor)
+    {
+        _carrinhoCompraAppService = carrinhoCompraAppService;
+        _httpContextAccessor = contextAccessor;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var session = _httpContextAccessor.HttpContext?.Session;
+
+        var carrinhoCompraId = session.GetString("CarrinhoCompraId");
+        var _carrinhoCompra = _carrinhoCompraAppService.ObterCarrinhoCompra(carrinhoCompraId);
+
+        return View(new CarrinhoCompraViewModel() { CarrinhoCompra = _carrinhoCompra });
+    }
+}
